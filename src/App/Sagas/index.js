daa240449d8b5c3417c1d7b1863ca7ece41baf97
Mathias Karner <karner@iiasa.ac.at>
2019-01-07 23:48:45 +0100

    Added login saga

--- src/App/Sagas/index.js
+++ src/App/Sagas/index.js
@@ -1,6 +1,9 @@
 import { all, takeLatest } from "redux-saga/effects";
+import { SEND_LOGIN } from "../Lib/User/Reducers/ActionTypes";
+import { sendLogin } from "../Lib/User/Sagas";
 
 export default function * root() {
   yield all([
+    takeLatest(SEND_LOGIN, sendLogin)
   ]);
 }
